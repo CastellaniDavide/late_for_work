0769f98fd56536de21b905dda2e2cc6f7b8ee0ee
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-08-28 16:25:29 +0200

    Created bin/template.cpp

---
+++ bin/late_for_work.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file late_for_work.cpp
+ *
+ * @version 01.01 2020-8-28
+ *
+ * @brief https://training.olinfo.it/#/task/ois_time/statement
+ *
+ * @ingroup late_for_work
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+//  uncomment the following lines if you want to read/write from files
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  cin >> N;
+
+  return 0;
+}
